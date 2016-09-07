a181ec9b05a606e6932e788816d6b26101da86e3
Sliqric7053 <isloan@ISLOAN.local>
2016-09-07 14:00:30 +0200

    Add logic to main.js
    Added:
    Handling to empty string search and null results
    Handling for Keyboard Enter key

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
 document.getElementById('submit-btn').onclick = searchWiki;
 
 function searchWiki() {
-
 // declare all required variables
 var inputValue  = document.getElementById('input').value;
-var generateBtn = document.getElementById('generate-btn');
 var displayArea = document.getElementById('results-section');
+var ul          = document.getElementById('ul');
+var description = document.getElementById('description');
 var xmlhttp     = new XMLHttpRequest();
-var randomUrl   = 'https://en.wikipedia.org/wiki/Special:Random';
 var wikiUrl     = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + inputValue + '&limit=10&format=json&origin=*';
-console.log('wikiUrl: ' + wikiUrl);
 
 //The XMLHttpRequest.readyState property returns the state an XMLHttpRequest client is in. (see: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readyState)
 xmlhttp.onreadystatechange = function() {
   if (xmlhttp.readyState == 4 && xmlhttp.status == 200) { //4 means The operation is complete.
     var wikiResponse = JSON.parse(xmlhttp.responseText);
-    console.log('wikiResponse: ' + wikiResponse);
-    displayWikiArticles(wikiResponse);
+    if (wikiResponse[1]) {
+      //clear old data
+      displayArea.innerHTML = '';
+      displayWikiArticles(wikiResponse);
+    } else if (!wikiResponse[1]) {
+      displayArea.innerHTML = '<h1>"Enter Search Term"!</h1>';
+    } else {
+      displayArea.innerHTML = '<h1>"Results Not Found"!</h1>';
+    }
   };
 };
 
@@ -26,15 +31,20 @@ xmlhttp.open('GET', wikiUrl, true);
 xmlhttp.send();
 
 function displayWikiArticles(arr) {
-  var name        = '';
-  var description = '';
-  var link        = '';
-  if ( typeof(arr) == 'object') {
-    for (var i = 0; i < arr.length; i++) {
-      printOut += '<p class="displayPara"><a href="' + arr[3][0] + '"' + '</a>' + arr[i][0] + '</p><br>';
-    }
-  }
-  displayArea.innerHTML = printOut;
-}
 
-}
+  var printOut  = '';
+  for (var i = 0; i < arr[1].length; i++) {
+    printOut += "<li class='li'><a href=" + arr[3][i] + " target='_blank'>" + arr[1][i] + "</a><p>" + arr[2][i] + "</p></li>";
+  };
+      displayArea.innerHTML = printOut;
+    };
+document.getElementById('input').value = '';
+};
+
+//setup keboard Enter key
+function keyboardPress(event) {
+  if (event.keyCode == 13 || event.which == 13) {
+    event.preventDefault();
+    searchWiki();
+  };
+};
